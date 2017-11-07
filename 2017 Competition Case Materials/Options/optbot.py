8728ce17ac2c8a982be6686d3e33735f62b7c599
andyznew <akzhang@ucsd.edu>
2017-11-06 20:24:05 -0800

    smile start

--- 2017 Competition Case Materials/Options/optbot.py
+++ 2017 Competition Case Materials/Options/optbot.py
@@ -1,6 +1,7 @@
 import tradersbot as tt
 import time
 import mibian
+import math
 
 t = tt.TradersBot(host='127.0.0.1', id='trader0', password='trader0')
 
@@ -15,6 +16,7 @@ start = time.time()
 order_id = []
 info = []
 
+history = {} # ticker : [isBuy, quantity, price]
 
 def f(msg, order):
     global spot
@@ -46,7 +48,8 @@ def f(msg, order):
     print 'SPOOOT', spot
     vals()
 
-    cancelOrders(order)
+    smileTrade(order)
+    #cancelOrders(order)
 
 def vals():
     time_left = 450 - (time.time() - start)
@@ -58,14 +61,14 @@ def vals():
         prev = val.impliedVolatility
         print vols[call]
         # greeks
-        call_greeks[call] = (val.callDelta, val.vega, val.gamma)
+        call_greeks[call] = (val.impliedVolatility, val.callDelta, val.vega, val.gamma)
     prev = None
     for put in puts:
         val = mibian.BS([spot, put, 0, time_left/15.0], putPrice = puts[put], volatility = prev )
         vols[put] = val.impliedVolatility
         prev = val.impliedVolatility
         print vols[put]
-        put_greeks[put] = (val.putDelta, val.vega, val.gamma)
+        put_greeks[put] = (val.impliedVolatility, val.putDelta, val.vega, val.gamma)
 
 def cancelOrders(order):
         global order_id
@@ -89,6 +92,49 @@ def h(msg, order):
                         #print('iiddd')
                         #print(k['order_id'])
                         info.append(k['ticker'])
+
+
+def smileTrade(order):
+    #global put_greeks
+    global call_greeks
+    global spot
+    index = 80
+    difference = 1000
+    ll = []
+    for k in call_greeks:
+        ll.append(k)
+    ll = sorted(ll)
+    for i in range(len(ll)):
+        diff = abs(spot - call_greeks[ll[i]][0])
+        if diff < difference:
+                index = i
+                difference = diff
+    print('eeeereirjeijriejrieji')
+    print call_greeks
+    print 'doodoooododoo'
+    for i in range(index, len(ll) - 1):
+        #if the volatility
+        print call_greeks[ll[i]][0]
+        if ( call_greeks[ll[i+1]][0] < call_greeks[ll[i]][0]):
+            ticker = "T"+str(i+1)+"C"
+            makeTrade(ticker, True, 1000, calls[ll[i+1]]*1.05, order)
+    for i in range(index, 1):
+        #if the volatility
+        print call_greeks[ll[i]][0]
+        if ( call_greeks[ll[i-1]][0] < call_greeks[ll[i]][0]):
+            ticker = "T"+str(i-1)+"C"
+            makeTrade(ticker, True, 1000, calls[ll[i-1]]*1.05, order)
+    print 'dooooon'
+    for i in range(len(ll)):
+        print ll[i]
+
+
+def makeTrade(ticker, isBuy, quantity, price, order):
+        if ticker not in history:
+                history[ticker] = []
+        history[ticker].append([isBuy, quantity, price])
+        order.addTrade(ticker, isBuy, quantity, price)
+
 
 t.onMarketUpdate = f
 #t.onTrade = g
